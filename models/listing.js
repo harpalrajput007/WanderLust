574f1b3d8e6239d182ebb924fa9651e43c2bdf21
Harpal Rajput <harpalsinghrajput007@gmail.com>
2025-04-26 20:46:28 +0530

    Project Phase 1 (part b)

--- models/listing.js
+++ models/listing.js
@@ -1,20 +1,21 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const listingSchema = new Schema ({
-    title:{
-      type : String, 
-      required : true,
-    }, 
-    description: String,
-    image:{
-       type: Object, 
-       default: "https://www.istockphoto.com/photo/coconut-palm-trees-on-tropical-island-beach-at-colorful-sunset-gm1443611234-482534833",
-       set: (v) => v === "" ? "https://www.istockphoto.com/photo/coconut-palm-trees-on-tropical-island-beach-at-colorful-sunset-gm1443611234-482534833" : v,
-    }, 
-    price: Number,
-    location: String,
-    country: String
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+   filename:String,
+    url:{ type: String,
+    default: "https://images.unsplash.com/photo-1586375300773-8384e3e4916f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGxvZGdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    set: (v) => v === "" ? "https://images.unsplash.com/photo-1586375300773-8384e3e4916f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGxvZGdlfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60" : v,}
+  },
+  price: Number,
+  location: String,
+  country: String
 });
 
 const listing = mongoose.model("listing", listingSchema);
